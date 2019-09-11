c52e1500ea61107ce8d06582dc58456383e2a104
Johan Swanepoel <johanswanepoel@Johans-MacBook-Pro.local>
2019-09-11 20:40:15 +1000

    Adding views

---
+++ management_ui/apiviews/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import DetailView, RedirectView, UpdateView
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+
