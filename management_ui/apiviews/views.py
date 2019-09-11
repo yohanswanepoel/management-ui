79d2e7258fe3adb4f60e336d2ef0dd765cfa598e
Johan Swanepoel <johanswanepoel@Johans-MacBook-Pro.local>
2019-09-11 20:53:46 +1000

    Code should always run

--- management_ui/apiviews/views.py
+++ management_ui/apiviews/views.py
@@ -1,9 +1,12 @@
+from services import *
 from django.urls import reverse
 from django.views.generic import DetailView, RedirectView, UpdateView
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 from django.http import HttpResponse
-import services
+from django.shortcuts import get_object_or_404, render
 
 def main_page(request):
-    pass
+    get_count_parties = get_count_reliant_parties()
+    # Need to define HTML
+    return render(request,'books.html', get_count_parties)
