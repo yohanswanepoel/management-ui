22b7c5135328481b6fda62b30a3b9275bd0ce9c0
Johan Swanepoel <johanswanepoel@Johans-MacBook-Pro.local>
2019-09-11 20:48:57 +1000

    adding views that call APIs

---
+++ management_ui/apiviews/services.py
@@ -0,0 +1,8 @@
+
+def get_count_reliant_parties():
+    url = 'http://127.0.0.1:8000/api/v1/reliantparties/count' 
+    #params = {'year': year, 'author': author}
+    r = requests.get(url, params=params)
+    parties = r.json()
+    parties_list = {'reliant_parties':parties['results']}
+    return parties_list
--- management_ui/apiviews/views.py
+++ management_ui/apiviews/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.views.generic import DetailView, RedirectView, UpdateView
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
+from django.http import HttpResponse
 
+
+def main_page(request):
+    
