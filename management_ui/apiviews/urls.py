0aa73cd520d145ecaaf2b0f98b3659159a63bca8
Johan Swanepoel <johanswanepoel@Johans-MacBook-Pro.local>
2019-09-12 20:06:19 +1000

    readability

--- management_ui/apiviews/urls.py
+++ management_ui/apiviews/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
 from . import views
 
-from management_ui.users.views import (
-    user_redirect_view,
-    user_update_view,
-    user_detail_view,
-)
-
 app_name = "apiviews"
 urlpatterns = [
     path("", views.main_page, name="main"),
